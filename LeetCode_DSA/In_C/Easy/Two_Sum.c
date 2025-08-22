e068c05a2a2ef636c686a27106262e28c8138998
Prakhar Pandey <studiousprakhar2260@gmail.com>
2025-08-22 23:13:08 +0530

    Update Two_Sum.c
    THIS IS A CUSTOM APPROACH BY ME WHICH BASICALLY USES BRUTE FORCE TO FIND THE PAIRS.THIS CODE WILL NOT WORK ON LEETCODE AS THERE A DEFINED FUNCTION AND MAIN IS ALREADY THERE.

--- LeetCode_DSA/In_C/Easy/Two_Sum.c
+++ LeetCode_DSA/In_C/Easy/Two_Sum.c
@@ -1,1 +1,38 @@
+# include <stdio.h>
 
+int main()
+{
+ int n;
+
+ printf("ENTER LENGTH OF ARRAY\n");
+ scanf("%d",&n);
+
+ int ar[n];
+
+ printf("ENTER ARRAY ELEMENTS\n");
+
+ for(int i=0;i<n;i++)
+ {
+   scanf("%d",&ar[i]); 
+ }
+ 
+ int target;
+ printf("ENTER THE TARGET ELEMENTS\n");
+ scanf("%d",&target);
+
+ for(int i=0;i<n;i++)
+   {
+    for(int j=i+1;j<n;j++)
+      {
+       if((ar[i]+ar[j])==target)
+         {
+           printf("[%d,%d]",i,j); 
+           break; 
+         }
+      }
+   }
+
+
+
+  return 0;
+}
