8f0cf8509d522f8356df1ffa60f519a47005014d
Prakhar Pandey <studiousprakhar2260@gmail.com>
2025-08-27 23:01:00 +0530

    Update Selling_Coins.c
    Chef has 
    A
    A silver coins and 
    B
    B gold coins.
    
    He can avail the following 
    2
    2 deals (multiple times):
    
    Sell 
    1
    1 silver coin for Rs. 
    1
    1.
    Trade 
    1
    1 gold coin for 
    2
    2 silver coins.
    Chef is trying to sell all his coins and earn money now. How much money will Chef be able to earn?
    
    Input Format
    The first and only line of each test case contains 
    2
    2 integers - 
    A
    A and 
    B
    B.
    Output Format
    For each test case, output on a new line the amount of money Chef can earn.
    
    Constraints
    1
    ≤
    A
    ,
    B
    ≤
    10
    1≤A,B≤10
    Sample 1:
    Input
    Output
    2 1
    4
    Explanation:
    Chef can first trade his gold coin for 
    2
    2 silver coins. Now he has 
    4
    4 silver coins in total, and he sells all of them for Rs. 
    4
    4.
    
    Sample 2:
    Input
    Output
    10 10
    30

--- CodeChef_ContestQuestions/In_C/Starters_201(DIV-4)/Selling_Coins.c
+++ CodeChef_ContestQuestions/In_C/Starters_201(DIV-4)/Selling_Coins.c
@@ -1,1 +1,16 @@
 
+#include <stdio.h>
+
+int main() 
+{
+
+    int A,B;
+    scanf("%d %d",&A,&B);
+    
+    int Trade=2*B;
+    
+    int totalmoney=Trade+A;
+    printf("%d",totalmoney);
+    
+    return 0;
+}
